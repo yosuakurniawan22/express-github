43025756a711fd6c7602747533870f4284080716
yosuakurniawan22 <yosuakurniawan2322@gmail.com>
2023-07-24 21:12:23 +0700

    push

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import database from "./db/index.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+database.sync()
+.then(() => {
+  console.log('database synced');
+})
+.catch((err) => {
+  console.log('failed to sync database: ' + err.message);
+})
 
 app.get('/', (req, res) => {
   res.json({ test: "Halo Express"});
